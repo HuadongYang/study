19dbc7e47334d1bcfb929fa98a38323ee1df973d
yanghd <979078205@qq.com>
2018-01-31 15:04:24 +0800

    零配置文件的web工程

---
+++ study/noXmlWebProject/src/main/java/com/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class IndexController {
+
+	
+	@RequestMapping(value="/wel")
+	@ResponseBody
+	public String getName(@PathVariable Integer id){
+		return "noConfiguration welcome";
+	}
+	
+}
